173590d850df9af7033b6c247b7e6801974dfba4
PAVEL PETROV <doctorrosen2016@gmail.com>
2020-03-31 22:32:32 +0600

    work with admin panel#4

--- src/js/admin.js
+++ src/js/admin.js
@@ -1,7 +1,26 @@
-function addAuto() {
-    let list = document.getElementsByClassName('node');
-    // list.insertAdjacentElement("beforeend", option);
-    alert(list.length);
-    console.log(list);
-}
+window.addEventListener('load', function () {
+    let chooseButtonAdd = document.querySelector('.btnAdd'),
+        chooseButtonDel = document.querySelector('.btnDelete'),
+        car = document.getElementById('carList'),
+        firebaseConfig = {};
+
+    chooseButtonAdd.addEventListener('click', function () {
+        let option = new Option('Новый автомобиль');
+        car.append(option);
+        option.selected = true;
+    });
+
+    chooseButtonDel.addEventListener('click', function () {
+        let selectedOption = car.options[car.selectedIndex];
+        selectedOption.remove();
+    });
+
+    firebase.initializeApp(firebaseConfig);
+});
+
+
+
+
+
+
 
